f32a8d68864578c432187215b4176ad690062950
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2024-10-14 18:17:54 +0530

    Admin setup

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ import AdminLogin from './admin/Login/Login.jsx';
 import AdminLayout from './admin/Layout/AdminLayout.jsx';
 import Login from './user/Page/SignUp/Login.jsx';
 import SignUp from './user/Page/SignUp/SignUp.jsx';
+import PaymentList from './admin/PaymentList/PaymentList.jsx';
+import UserList from './admin/UserList/userList.jsx';
+import BookingList from './admin/Bookingmanagement/BookingList.jsx';
 
 function App() {
   return (
@@ -17,13 +20,16 @@ function App() {
         <Routes>
           {/* User */}
           <Route path="/" element={<Index />} />
-          <Route path="/login" element={<Login/>}/>
-          <Route path='/sign-up' element={<SignUp/>}/>
-          <Route path="/about" element={ <UserLayout>    <About /> </UserLayout> } />
+          <Route path="/login" element={<Login />} />
+          <Route path='/sign-up' element={<SignUp />} />
+          <Route path="/about" element={<UserLayout>    <About /> </UserLayout>} />
 
           {/* Admin */}
-          <Route path="/access-admin" element={     <AdminLayout>   <AdminIndex />          </AdminLayout> } />
-          <Route path='/access-admin/login' element={  <AdminLogin />  } />
+          <Route path="/access-admin" element={<AdminLayout>   <AdminIndex />          </AdminLayout>} />
+          <Route path='/access-admin/login' element={<AdminLogin />} />
+          <Route path="/access-admin/payment-list" element={<AdminLayout> <PaymentList /> </AdminLayout>} />
+          <Route path="/access-admin/user-list" element={<AdminLayout> <UserList />  </AdminLayout>} />
+          <Route path="/access-admin/book-list" element={<AdminLayout> <BookingList />  </AdminLayout>} />
         </Routes>
       </Router>
     </>
