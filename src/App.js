d1be8729d729ae81b5fee918bb2fb0b93f99b0bb
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2024-10-15 10:27:00 +0530

    User Setup

--- src/App.js
+++ src/App.js
@@ -2,11 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import Index from "./user/Page/Home/Index.jsx"
 import About from "./user/Page/About/About.jsx"
-import AdminIndex from "./admin/Dashboard/Index.jsx"
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import UserLayout from './user/Layout/UserLayout.jsx';
-import AdminLogin from './admin/Login/Login.jsx';
-import AdminLayout from './admin/Layout/AdminLayout.jsx';
 import Login from './user/Page/SignUp/Login.jsx';
 import SignUp from './user/Page/SignUp/SignUp.jsx';
 
