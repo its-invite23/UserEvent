79baab2c6ed07346488cf687718abf52ff537a6d
fpmukesh <mukesh@fpdemo.com>
2024-10-17 09:43:06 +0530

    User Login Signup Start Responsive working

--- src/user/compontents/Header.jsx
+++ src/user/compontents/Header.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import logo from "../../assets/logo.png"
+import { RiMenu3Line } from "react-icons/ri";
 
 export default function Header() {
   return (
-    <div className='flex items-center justify-between max-w-[1180px] m-auto'>
+    <div className='flex items-center justify-between max-w-[1180px] m-auto px-[15px]'>
       <Link to={"/"}>
         <img src={logo} alt="" />
       </Link>
       <div className='flex items-center'>
-        <ul className='flex items-center gap-[36px] md:gap-[20px] lg:gap-[36px] font-manrope font-[600] text-[15px] md:text-[18px] lg:text-[20px] text-white'>
+        <button className='absolute right-[250px] lg:hidden'><RiMenu3Line className='text-white text-[35px]' /></button>
+        <ul className='absolute top-[90px] right-[220px] lg:relative flex flex-col lg:flex-row items-center gap-[36px] md:gap-[20px] lg:gap-[36px] font-manrope font-[600] text-[15px] md:text-[18px] lg:text-[20px] text-white'>
           <li>
             <Link to={"/"}>Home</Link>
           </li>
