d454cba947b6ed7a0c29a9b91e2c9230e02a2e27
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2024-11-22 14:46:41 +0530

    get

--- src/user/Page/Payment/Success.jsx
+++ src/user/Page/Payment/Success.jsx
@@ -7,7 +7,7 @@ export default function Success() {
   const navigate = useNavigate();
 
   const handleBackToHome = () => {
-    navigate('/'); 
+    navigate('/');
   };
 
   const { id } = useParams();
@@ -23,35 +23,35 @@ export default function Success() {
       });
   };
 
-  useEffect(()=>{
-    if(id){
-        fetch(id);
+  useEffect(() => {
+    if (id) {
+      fetch(id);
     }
-  },[])
+  }, [])
 
   return (
     <div className='bg-[#000] p-[10px] h-full min-h-full'>
-    {/* <AuthLayout> */}
-   <div className="flex flex-col items-center bg-[#000000] justify-center min-h-screen  p-4">
-      <div className="text-center">
-        <img
-          src={PaymentSuccess} 
-          alt="Booking Successful"
-          className="mb-4 rounded-lg  object-cover mx-auto"
-        />
-        <h1 className="font-manrope font-[700] text-[25px] leading-[30px] sm:text-[30px] sm:leading-[40px] md:text-[48px] md:leading-[50px] text-white mb-[15px]">Your booking has been successful</h1>
-        <p className="text-[#FFFFFF] m-auto mb-4 w-full max-w-[420px]">
-        Thank you for placing your booking request with us.
-        We’ve received your request and will confirm you shortly.
-        </p>
-        <button
-          onClick={handleBackToHome}
-          className="w-[180px] md:w-[96%] max-w-[334px]  mt-4 px-[10px] py-[12px] md:py-[20px] bg-[#EB3465] font-manrope font-[700] text-[15px] md:text-[20px] text-white rounded-[4px] hover:bg-[#fb3a6e] transition duration-300"
-        >Back to home
-        </button>
+      {/* <AuthLayout> */}
+      <div className="flex flex-col items-center bg-[#000000] justify-center min-h-screen  p-4">
+        <div className="text-center">
+          <img
+            src={PaymentSuccess}
+            alt="Booking Successful"
+            className="mb-4 rounded-lg  object-cover mx-auto"
+          />
+          <h1 className="font-manrope font-[700] text-[25px] leading-[30px] sm:text-[30px] sm:leading-[40px] md:text-[48px] md:leading-[50px] text-white mb-[15px]">Your booking has been successful</h1>
+          <p className="text-[#FFFFFF] m-auto mb-4 w-full max-w-[420px]">
+            Thank you for placing your booking request with us.
+            We’ve received your request and will confirm you shortly.
+          </p>
+          <button
+            onClick={handleBackToHome}
+            className="w-[180px] md:w-[96%] max-w-[334px]  mt-4 px-[10px] py-[12px] md:py-[20px] bg-[#EB3465] font-manrope font-[700] text-[15px] md:text-[20px] text-white rounded-[4px] hover:bg-[#fb3a6e] transition duration-300"
+          >Back to home
+          </button>
+        </div>
       </div>
-    </div>
-    {/* </AuthLayout> */}
+      {/* </AuthLayout> */}
     </div>
 
   )
