89e708ccc494c0e52dea554595cfdd75435e1af6
fpmukesh <mukesh@fpdemo.com>
2024-11-26 12:29:11 +0530

    Event orginizer page update

--- src/user/Page/EventOrganiser/Organiser.jsx
+++ src/user/Page/EventOrganiser/Organiser.jsx
@@ -29,7 +29,7 @@ export default function Organiser() {
   return (
     <div className="bg-[#000] h-full min-h-full">
       <UserLayout>
-        <div className="container">
+        <div className="">
           <div className=" w-full max-w-[950px] m-auto pt-[60px] lg:pt-[100px] mb-[80px] px-[15px]">
             <h1 className="text-[30px] md:text-[80px] lg:text-[90px] text-center text-white font-[700] mb-[20px] md:mb-[40px]">
               Imagine. <span className="scribble">Plan</span>. Book.
