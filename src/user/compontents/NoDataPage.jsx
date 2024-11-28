b97f32368652bce2b2cafb090ca1c03225063834
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2024-11-28 16:26:47 +0530

    manage

--- src/user/compontents/NoDataPage.jsx
+++ src/user/compontents/NoDataPage.jsx
@@ -16,7 +16,7 @@ const NoDataPage = () => {
       </p>
       <button
         onClick={() => window.location.reload()}
-        className="mt-6 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+        className="mt-6 px-4 py-2 bg-[#ff0062] hover:bg-[#4400c3] text-white rounded"
       >
         Back To Pervious Page
       </button>
