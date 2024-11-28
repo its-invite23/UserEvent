e33e9a6419e174f9ce6114ce6a68eb5934c9936e
fpmukesh <mukesh@fpdemo.com>
2024-11-28 15:09:56 +0530

    QA Feedback Done

--- src/user/Page/Payment/StripePayment.js
+++ src/user/Page/Payment/StripePayment.js
@@ -57,17 +57,17 @@ const StripePayment = () => {
     }
   }, [])
 
-  return <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-6">
-    <div className="bg-white shadow-md rounded-lg p-8 max-w-md text-center">
-      <h1 className="text-2xl font-bold text-gray-800 mb-4">Complete Your Payment</h1>
-      <p className="text-gray-600 mb-6">
+  return <div className="flex flex-col items-center justify-center min-h-screen bg-black  p-6">
+    <div className="bg-black shadow-md  rounded-md  border  border-white-600 text-white rounded-lg p-8 max-w-md text-center">
+      <h1 className="text-2xl font-bold  mb-4">Complete Your Payment</h1>
+      <p className=" mb-6">
         Click the button below to securely process your payment.
       </p>
       <button
         onClick={() => {
           handlePayment();
         }}
-        className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-3 px-6 rounded-lg shadow-md transition-all duration-300 ease-in-out transform hover:scale-105 focus:ring focus:ring-blue-300 focus:outline-none"
+        className=" bg-[#ff0062] hover:bg-[#4400c3] text-white font-semibold py-3 px-6 rounded-lg shadow-md transition-all duration-300 ease-in-out transform hover:scale-105 focus:ring focus:ring-blue-300 focus:outline-none"
       >
         Pay Now
       </button>
