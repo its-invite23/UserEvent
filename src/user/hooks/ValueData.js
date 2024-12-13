fcda6b677e59922df80a603d55420f060ec7ca11
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2024-12-13 15:03:19 +0530

    manage Package and booking design

--- src/user/hooks/ValueData.js
+++ src/user/hooks/ValueData.js
@@ -4,8 +4,8 @@ export const formatMultiPrice = (amount, currency) => {
   const formattedValue = new Intl.NumberFormat("en-GB", {
     style: "currency",
     currency: currency,
-    minimumFractionDigits: 0,
-    maximumFractionDigits: 0,
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
   }).format(amount);
 
   return formattedValue.replace(/(\D+)(\d)/, '$1 $2');
