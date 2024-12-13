a6b7177d24c0ba413d1f9bc7b9443d6c13b008bd
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2024-12-13 15:09:35 +0530

    manage Package and booking design

--- src/user/Page/services/ServicesProvider.jsx
+++ src/user/Page/services/ServicesProvider.jsx
@@ -145,7 +145,7 @@ export default function ServicesProvider({ data, description }) {
             })}
           </div>
         </div>
-        {filteredServices && filteredServices && filteredServices.length > 0 ? (
+        {firstItem && firstItem && firstItem.length > 0 ? (
           <>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4">
               {firstItem?.map((venue, index) => (
