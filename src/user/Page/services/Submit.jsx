8c3ebf5c28c4996ed43530eafa1e0260a25e655e
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2025-01-03 17:31:39 +0530

    file name

--- src/user/Page/services/Submit.jsx
+++ src/user/Page/services/Submit.jsx
@@ -6,7 +6,34 @@ import { clearData } from "../Redux/formSlice.js";
 import Listing from "../../../Api/Listing";
 import toast from "react-hot-toast";
 import { CurrencyContext } from "../../../CurrencyContext.js";
+import Popup from "../../compontents/Popup.jsx";
+import LoginLogic from "../SignUp/LoginLogic.jsx";
+import SignUpPopupLogic from "../SignUp/SignUpPopupLogic.jsx";
+import VerifyOTP from "../SignUp/VerifyOTP.jsx";
+
 export default function Submit() {
+
+    const token = localStorage && localStorage.getItem("token");
+    const [isPopupOpen, setIsPopupOpen] = useState(false);
+    const [isSignUpPopupOpen, setIsSignUpPopupOpen] = useState(false);
+    const [isOTPPopupOpen, setIsOTPPopupOpen] = useState(false);
+
+    const openPopup = () => setIsPopupOpen(true);
+    const closePopup = () => {
+        setIsPopupOpen(false)
+    };
+    const closeLoginOpenSignUp = () => {
+        setIsSignUpPopupOpen(true);
+        setIsPopupOpen(false)
+    };
+    const closeSignUpPopup = () => setIsSignUpPopupOpen(false);
+
+    const closeSignUpOpenOTPPopup = () => {
+        setIsOTPPopupOpen(true);
+        setIsSignUpPopupOpen(false);
+    }
+
+    const closeOTPPopup = () => setIsOTPPopupOpen(false);
     const dispatch = useDispatch();
     const updatedFormData = useSelector((state) => state.form.updatedFormData);
 
@@ -59,16 +86,43 @@ export default function Submit() {
                 But don’t worry! You can still submit your request, and our team will personally get back to you within the next hour with tailored suggestions to make your event exceptional.
             </p>
             <div className="mt-[30px]">
-                <div
-                    onClick={handleSubmit}
-                    className={`mt-4 px-[50px] py-[17px] font-[500] text-[18px] rounded transition duration-300 bg-[#ff0062] text-white hover:bg-[#4400c3] 
-                          cursor-pointer
-                        }`}
-
+                <button
+                    onClick={() => {
+                        if (token) {
+                            handleSubmit();
+                        } else {
+                            // Open popup
+                            openPopup();
+                        }
+                    }}
+                    className="px-[25px] py-[12px] xl:px-[30px] xl:py-[15px] bg-[#ff0062] hover:bg-[#4400c3] font-manrope font-[500] text-[16px] lg:text-[18px] text-white rounded-[5px]"
                 >
-                    {loading ? "Loading..." : "Submit"}
-                </div>
+                    {loading ? "Processing..." : " Request to book"}
+                </button>
             </div>
+            <Popup
+                isOpen={isPopupOpen}
+                onClose={closePopup}
+                size="w-full max-w-lg"
+                content={<LoginLogic isPopup={true} onClose={closePopup} closeLoginOpenSignUp={closeLoginOpenSignUp} />}
+            />
+            {/* Sign Up */}
+            <Popup
+                isOpen={isSignUpPopupOpen}
+                size="w-[70%]"
+                onClose={closeSignUpPopup}
+                content={<SignUpPopupLogic isPopup={true} onClose={closeSignUpOpenOTPPopup} />}
+            />
+
+            {/* OTP */}
+            <Popup
+                isOpen={isOTPPopupOpen}
+                onClose={closeOTPPopup}
+                // content={<SignUpPopupLogic isPopup={true} onClose={closeSignUpPopup}/>}
+                size="max-w-lg"
+                content={<VerifyOTP onClose={closeOTPPopup} />}
+            />
+
         </div>
     )
 }
