8a41ead4b2d01834a256388d4b538e96763b7c39
its-invite23 <contact@its-invite.com>
2025-05-31 17:07:25 +0100

    Update Final Step Button

--- src/user/Page/GetStart/NextPreBtn.jsx
+++ src/user/Page/GetStart/NextPreBtn.jsx
@@ -6,14 +6,21 @@ export default function NextPreBtn({ onPrev, onNext, currentStep }) {
     <div className='flex items-center gap-[10px] md:gap-[20px]'>
       <button
         className="flex items-center justify-center gap-[8px] w-[100%] min-w-[120px] md:min-w-[140px] px-[10px] py-[12px] md:py-[14px] rounded-[60px] border border-[#fff] bg-[#141414] hover:bg-[#4400c3] font-[manrope] font-[600] text-[13px] md:text-[16px] text-white text-center"
-        onClick={onPrev}  // Attach the onPrev function
+        onClick={onPrev}
       >
         <FaArrowLeft /> Prev
       </button>
-      {currentStep !== 10 && (
+      {currentStep === 10 ? (
         <button
-          className="flex items-center justify-center gap-[8px] w-[100%] min-w-[120px] md:min-w-[140px]  px-[10px] py-[12px] md:py-[14px] border border-[#ff0062]  hover:border hover:border-[#4400c3] rounded-[60px] bg-[#ff0062] hover:bg-[#4400c3] font-[manrope] font-[600] text-[13px] md:text-[16px] text-white text-center"
-          onClick={onNext}  // Attach the onNext function
+          className="flex items-center justify-center gap-[8px] w-[100%] min-w-[120px] md:min-w-[140px] px-[10px] py-[12px] md:py-[14px] border border-[#ff0062] hover:border hover:border-[#4400c3] rounded-[60px] bg-[#ff0062] hover:bg-[#4400c3] font-[manrope] font-[600] text-[13px] md:text-[16px] text-white text-center"
+          onClick={onNext}
+        >
+          Get Started <FaArrowRight />
+        </button>
+      ) : (
+        <button
+          className="flex items-center justify-center gap-[8px] w-[100%] min-w-[120px] md:min-w-[140px] px-[10px] py-[12px] md:py-[14px] border border-[#ff0062] hover:border hover:border-[#4400c3] rounded-[60px] bg-[#ff0062] hover:bg-[#4400c3] font-[manrope] font-[600] text-[13px] md:text-[16px] text-white text-center"
+          onClick={onNext}
         >
           Next <FaArrowRight />
         </button>
